91386df6bdc6b7a34692352cdb29c1afc22f11de
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-03-09 20:12:58 -0300

    update: create api cards

---
+++ be_projectmap/app/Http/Controllers/cardApi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\cards;
+use Illuminate\Http\Request;
+
+class cardApi extends Controller
+{
+    function getCard(){
+        return cards::all();
+    }
+}
