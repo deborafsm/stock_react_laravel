103813908597ad2540a263891bebf480d5dd58ad
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-03-16 22:42:18 -0300

    Update api.php

--- be_projectmap/routes/api.php
+++ be_projectmap/routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\cardApi;
+use App\Http\Controllers\memberController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,9 +19,11 @@ use App\Http\Controllers\cardApi;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get("card",[cardApi::class ,'getCard']);
-Route::get("card_id/{id?}",[cardApi::class ,'getCardId']);
-Route::post("cardAdd",[cardApi::class ,'addCard']);
-Route::post("update",[cardApi::class ,'upCard']);
-Route::post("delete/{id?}",[cardApi::class ,'delCard']);
-Route::get("search/{tag}",[cardApi::class,'findCard']);
+Route::get("card", [cardApi::class, 'getCard']);
+Route::get("card_id/{id?}", [cardApi::class, 'getCardId']);
+Route::post("cardAdd", [cardApi::class, 'addCard']);
+Route::post("update", [cardApi::class, 'upCard']);
+Route::post("delete/{id?}", [cardApi::class, 'delCard']);
+Route::get("search/{tag}", [cardApi::class, 'findCard']);
+
+Route::apiResource("member", memberController::class);
