bb0f3062f783d3d613a06a9e0acf5de9200bce03
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-03-28 22:25:05 -0300

    create: model and controller mouse

---
+++ be_projectmap/app/Http/Controllers/Controllers/Mouse/mouseController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Controllers\Mouse;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class mouseController extends Controller
+{
+    //
+}
---
+++ be_projectmap/app/Models/Mouse/mouse.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Mouse;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class mouse extends Model
+{
+    use HasFactory;
+}
