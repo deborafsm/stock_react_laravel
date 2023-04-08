326a487d76c8732268a404b606133d82a99fdcc4
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-04-07 21:40:35 -0300

    create: model e controller webcam

---
+++ be_projectmap/app/Http/Controllers/Controllers/WebCam/webcamController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Controllers\WebCam;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class webcamController extends Controller
+{
+    //
+}
---
+++ be_projectmap/app/Models/WebCam/webcam.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\WebCam;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class webcam extends Model
+{
+    use HasFactory;
+}
