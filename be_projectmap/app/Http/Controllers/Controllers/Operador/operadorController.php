4cac3f094d720783a48c69c5faaebc9bfbcb156f
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-04-17 22:20:40 -0300

    update:  function get operador

--- be_projectmap/app/Http/Controllers/Controllers/Operador/operadorController.php
+++ be_projectmap/app/Http/Controllers/Controllers/Operador/operadorController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Controllers\Operador;
 
 use App\Http\Controllers\Controller;
+use App\Models\Operador\operador;
 use Illuminate\Http\Request;
 
 class operadorController extends Controller
 {
-    //
+    function getOperador()
+    {
+        return operador::all();
+    }
 }
