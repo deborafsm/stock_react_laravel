dad1306e2bd3c36749ba601f0078c4bb7155e80b
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-04-27 21:41:26 -0300

    add: crontroller and model pc def

---
+++ be_projectmap/app/Http/Controllers/Controllers/Pc/pcDefController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Controllers\Pc;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class pcDefController extends Controller
+{
+    //
+}
---
+++ be_projectmap/app/Models/Pc/pcDefModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Pc;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pcDefModel extends Model
+{
+    use HasFactory;
+}
