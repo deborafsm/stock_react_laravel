cae1fe93d9998845d469976765bd6a37742afaf9
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-05-03 20:45:44 -0300

    create: model andcontroller kit

---
+++ be_projectmap/app/Models/Kit/kitModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Kit;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class kitModel extends Model
+{
+    use HasFactory;
+}
