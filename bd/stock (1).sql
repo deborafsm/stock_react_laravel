46ba598d41edc8a1d7e271bbdb4842e6027bb5d8
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-05-15 22:58:48 -0300

    Update stock (1).sql

--- bd/stock (1).sql
+++ bd/stock (1).sql
@@ -1,33 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 25-Abr-2023 às 00:13
--- Versão do servidor: 10.4.21-MariaDB
--- versão do PHP: 8.0.11
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `stock`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `defeitopc`
---
-
-CREATE TABLE `defeitopc` (
+CREATE TABLE `pcdeifeito` (
   `id` int(11) NOT NULL,
   `data_def` varchar(500) DEFAULT NULL,
   `descricao` varchar(500) DEFAULT NULL,
