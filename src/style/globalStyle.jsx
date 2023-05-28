8b13212633484e1f50403aed6b1cea6ce010fc3c
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-05-27 22:48:23 -0300

    Create globalStyle.jsx

---
+++ src/style/globalStyle.jsx
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    html, body, #root {
+        height: 100%;
+    }
+
+    *, button, input{
+        border: 0;
+        outline: 0;
+        font-family:monospace;
+    }
+    button {
+        cursor: pointer;
+    }
+    .container-title{
+        display: flex;
+        align-items: center;
+        align-items: center;
+        flex-direction: row-reverse;
+        justify-content: space-between;
+}
+    }
+`;
