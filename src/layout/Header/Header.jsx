ca55b6895646e5826b0225211bfe7c7466e63f41
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-05-27 22:49:38 -0300

    Create Header.jsx

---
+++ src/layout/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Container from '../Container/styleContainer'
+import { ContainerHeader } from './styleHeader'
+export default function Header() {
+    return (
+        <ContainerHeader></ContainerHeader>
+    )
+}
