a3df13412ff652c6fb470461415ae157c40507b9
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-05-28 21:22:00 -0300

    Create styleHeader.js

---
+++ src/layout/Header/styleHeader.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ContainerHeader = styled.div `
+    grid-area: HD;
+    background-color:white;
+    color: black;
+    padding-left: 15px;
+    height:100vh;
+  
+`
