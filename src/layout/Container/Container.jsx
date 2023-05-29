a6761979b7e380bb6f7868d07ca454092528d7ef
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-05-29 17:07:50 -0300

    Update Container.jsx

--- src/layout/Container/Container.jsx
+++ src/layout/Container/Container.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from './styleContainer.js'
+import { Container } from './styleContainer'
 export default function Content({ children }) {
     return (
         <>
