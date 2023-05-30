17fa36a81a489479a7fe0dbb375d751e928e8b6c
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-05-30 18:31:10 -0300

    Create DataAside.jsx

---
+++ src/layout/Aside/DataAside.jsx
@@ -0,0 +1,20 @@
+
+import {  MdHome } from "react-icons/md";
+
+export const SideData = [
+  {
+    id: "123",
+    title: "Home",
+    path: "/home",
+    icon: <MdHome />,
+    cName: "Home",
+  },
+  {
+    id: "123456",
+    title: "Info",
+    path: "/info",
+    icon: <MdHome />,
+    cName: "Home",
+  },
+  
+];
