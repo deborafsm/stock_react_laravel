61f8f4279195e9f2cc67c1e9d3007fb978918251
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-06-05 12:21:54 -0300

    Create index.jsx

---
+++ front_end/src/components/input/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './style.css'
+export default function index() {
+  return (
+    <div>
+      <div>
+        <label htmlFor="teste">teste</label>
+        <input type="text" className="input"/>
+      </div>
+    </div>
+  );
+}
