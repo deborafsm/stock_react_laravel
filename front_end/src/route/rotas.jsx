5e52025e4c716c8a73ee22ca051410596de903d1
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-06-11 21:12:15 -0300

    Update rotas.jsx

--- front_end/src/route/rotas.jsx
+++ front_end/src/route/rotas.jsx
@@ -3,14 +3,16 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Info from "../pages/Info";
-import WebCamCreate from "../pages/webcam/create"
+import WebCamCreate from "../pages/webcam/create";
+import HeadReadPage from "../pages/head/HeadReadPage.jsx"
 import Layout from "../layout/Grid/Layout";
 const Rotas = () => (
   <Layout>
     <Routes>
       <Route path="/home" element={<Home />} />
       <Route path="/info" element={<Info />} />
-      <Route path="/webcam-create" element={<WebCamCreate/>} />
+      <Route path="/webcam-create" element={<WebCamCreate />} />
+      <Route path="/heads" element={<HeadReadPage />} />
     </Routes>
   </Layout>
 );
