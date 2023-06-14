99d2da38086e453d95f75a27ef243e4758edc571
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-06-14 18:42:25 -0300

    Update rotas.jsx

--- front_end/src/route/rotas.jsx
+++ front_end/src/route/rotas.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Info from "../pages/Info";
 import WebCamCreate from "../pages/webcam/create";
-import HeadReadPage from "../pages/head/HeadReadPage.jsx"
+import HeadReadPage from "../pages/head/HeadRead.jsx";
 import Layout from "../layout/Grid/Layout";
 const Rotas = () => (
   <Layout>
