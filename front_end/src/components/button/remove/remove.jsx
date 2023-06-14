c3ce74afea9e76520b9b3643f4eaa51de7fcc7fa
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-06-14 20:05:38 -0300

    Update remove.jsx

--- front_end/src/components/button/remove/remove.jsx
+++ front_end/src/components/button/remove/remove.jsx
@@ -1,15 +1,29 @@
-import {  BsFillTrashFill} from "react-icons/bs";
+import { BsFillTrashFill } from "react-icons/bs";
 import styled from "styled-components";
 const StyledButton = styled.button`
-  background-color: red;
-  color: white;
-  padding: 2%;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  border: none;
   border-radius: 20px;
+  background-color: #dc3545;
+  color: #333;
   cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #ddd;
+  }
 `;
 
 function Remove() {
-  return <StyledButton><BsFillTrashFill/></StyledButton>;
+  return (
+    <StyledButton>
+      <BsFillTrashFill />
+    </StyledButton>
+  );
 }
 
 export default Remove;
