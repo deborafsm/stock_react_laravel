640ed91f7af0bcf004e447c1bb0c1df1145e80c9
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-06-15 23:13:46 -0300

    Create DataTable.jsx

---
+++ front_end/src/components/head/DataTable.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Edit from "../../components/button/remove/remove";
+import Remove from "../../components/button/edit/edit";
+import style from "../../style/style-head.css";
+
+import styled from "styled-components";
+const CenteredCell = styled.td`
+  display: flex;
+  -webkit-box-align: center;
+  justify-content: center;
+  align-items: center;
+`;
+
+function DataTable({ data, handleRemove, handleEdit }) {
+  return (
+    <tbody>
+      {data.map((item) => (
+        <tr key={item.id}>
+          <td>{item.id}</td>
+          <td>{item.marca}</td>
+          <td>{item.modelo}</td>
+          <td>{item.codigo}</td>
+          <td>{item.status}</td>
+          <CenteredCell>
+            <Remove onClick={() => handleRemove(item.id)}></Remove>
+            <Edit onClick={() => handleEdit(item.id)}></Edit>
+          </CenteredCell>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
+
+export default DataTable;
