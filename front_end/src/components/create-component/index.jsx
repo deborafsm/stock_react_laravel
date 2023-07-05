2593f51c3734ed8b7c280c8d4b3bd545c55df42a
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-07-05 11:37:04 -0300

    Update index.jsx

--- front_end/src/components/create-component/index.jsx
+++ front_end/src/components/create-component/index.jsx
@@ -1,6 +1,61 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
-function Create({ fields, onCreate }) {
+const FormContainer = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+const Notification = styled.div`
+  margin-top: 10px;
+  padding: 10px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: #fff;
+  border-radius: 4px;
+`;
+const FormTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const FormGroup = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+`;
+
+const SubmitButton = styled.button`
+  background-color: #28a745;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #218838;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 0.2rem rgba(40, 167, 69, 0.5);
+  }
+`;
+
+function Create({ fields, onCreate, notification }) {
   const [formData, setFormData] = useState({});
 
   const handleChange = (e, fieldName) => {
@@ -16,25 +71,32 @@ function Create({ fields, onCreate }) {
   };
 
   return (
-    <div>
-      <h2>Create Item</h2>
+    <FormContainer>
+      <FormTitle>Formulário Teclado</FormTitle>
       <form onSubmit={handleSubmit}>
         {fields.map((field) => (
-          <div key={field.name}>
-            <label>
-              {field.label}:
-              <input
-                type={field.type}
-                value={formData[field.name] || ""}
-                onChange={(e) => handleChange(e, field.name)}
-              />
-            </label>
-          </div>
+          <FormGroup>
+            <div key={field.id}>
+              <Label>
+                {field.label}:
+                <Input
+                  type={field.type}
+                  value={formData[field.name] || ""}
+                  onChange={(e) => handleChange(e, field.name)}
+                />
+              </Label>
+            </div>
+          </FormGroup>
         ))}
         <br />
-        <button type="submit">Create</button>
+        <SubmitButton type="submit">Create</SubmitButton>
+        {notification && (
+          <Notification backgroundColor={notification.backgroundColor}>
+            {notification.message}
+          </Notification>
+        )}
       </form>
-    </div>
+    </FormContainer>
   );
 }
 
