0bc409a2e7caebd66aa053e46381de91bb5300d1
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-07-10 15:20:39 -0300

    Update HeadCreate.jsx

--- front_end/src/pages/head/HeadCreate.jsx
+++ front_end/src/pages/head/HeadCreate.jsx
@@ -1,37 +1,51 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Create from "../../components/create-component/index";
-export let setNotification;
-const fields = [
-  { name: "marca", label: "Marca", type: "text" },
-  { name: "modelo", label: "Modelo", type: "text" },
-  { name: "codigo", label: "Código", type: "text" },
+export default function HeadCreate({ onCreate }) {
+  const [notification, setNotification] = useState(null);
+  const fields = [
+    { name: "marca", label: "Marca", type: "text" },
+    { name: "modelo", label: "Modelo", type: "text" },
+    { name: "codigo", label: "Código", type: "text" },
+  ];
 
-];
-
-const handleCreate = (formData) => {
-  fetch("http://127.0.0.1:8000/api/webcamAdd", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Lógica para tratar a resposta da API
-      console.log("Item criado com sucesso:", data);
+  const handleCreate = (formData) => {
+    fetch("http://127.0.0.1:8000/api/headAdd", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
     })
-    .catch((error) => {
-      // Lógica para tratar erros na chamada à API
-      console.error("Erro ao criar item:", error);
-    });
-};
-
-export default function HeadCreate() {
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result === "success") {
+          setNotification({
+            message: "Item criado com sucesso!",
+            backgroundColor: "#28a745",
+          });
+        } else {
+          setNotification({
+            message: "Erro ao criar item.",
+            backgroundColor: "#dc3545",
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erro ao criar item:", error);
+        setNotification({
+          message: "Erro ao criar item. Por favor, tente novamente mais tarde.",
+          backgroundColor: "#dc3545",
+        });
+      });
+  };
   return (
     <div>
-      <Create fields={fields} onCreate={handleCreate} />
+      <Create
+        title={"Head"}
+        fields={fields}
+        onCreate={handleCreate}
+        notification={notification}
+      />
     </div>
   );
 }
