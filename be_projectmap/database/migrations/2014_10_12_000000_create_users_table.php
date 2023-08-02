1ad38b4704e5a50016a4213f08ab95c8859ad889
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-08-01 21:26:59 -0300

    Update 2014_10_12_000000_create_users_table.php

--- be_projectmap/database/migrations/2014_10_12_000000_create_users_table.php
+++ be_projectmap/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nome');
+            $table->string('usuario')->unique();
+            $table->string('senha');
+            $table->boolean('adm')->default(false);
             $table->timestamps();
         });
     }
