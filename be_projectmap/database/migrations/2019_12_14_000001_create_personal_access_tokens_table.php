db56eb97bb5054fba5709b9a04d389141d1c1c71
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-08-04 19:08:17 -0300

    Update 2019_12_14_000001_create_personal_access_tokens_table.php

--- be_projectmap/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ be_projectmap/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -4,34 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
+            $table->string('nome')->default(''); // Adicione um valor padrão vazio ou outro valor padrão de sua preferência.
+            $table->string('usuario')->unique();
+            $table->string('senha');
+            $table->boolean('adm')->default(false);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('users');
     }
-};
+}
