85bb783f251e74d1ef69b949e2a50468ded47cf3
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2023-12-21 19:31:17 -0300

    Create update.jsx

---
+++ front_end/src/components/update-component/update.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import styled from "styled-components";
+const FormContainer = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+`;
+const Notification = styled.div`
+  margin-top: 10px;
+  padding: 10px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  color: #fff;
+  border-radius: 4px;
+`;
+const FormTitle = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
+
+const FormGroup = styled.div`
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+const Label = styled.label`
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+`;
+
+const SubmitButton = styled.button`
+  background-color: #28a745;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #218838;
+  }
+
+  &:focus {
+    outline: none;
+    box-shadow: 0 0 0 0.2rem rgba(40, 167, 69, 0.5);
+  }
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  margin: 0 auto;
+`;
+
+export default function Update({ fields, onUpdate, notification, title, dataHead, data }) {
+  const [formData, setFormData] = useState({});
+  const handleChange = (e, fieldName) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [fieldName]: e.target.value,
+    }));
+  };
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    onUpdate(formData);
+  };
+
+  return (
+    <div>
+      <FormContainer>
+        <FormTitle>{title}</FormTitle>
+        <form onSubmit={handleUpdate}>
+          <Grid>
+            {fields.map((field) => (
+              <FormGroup key={field.id}>
+                <Label>
+                  {field.label}:
+                  <Input
+                    type={field.type}
+                    defaultValue={data[field.name]}
+                    onChange={(e) => handleChange(e, field.name)}
+
+                  />
+                </Label>
+              </FormGroup>
+            ))}
+
+          </Grid>
+          <br />
+          <SubmitButton type="submit">Update</SubmitButton>
+          {notification && (
+            <Notification backgroundColor={notification.backgroundColor}>
+              {notification.message}
+            </Notification>
+          )}
+        </form>
+      </FormContainer>
+    </div>
+  )
+}
+
